63bb645120b757e86522cb68c58c7aa3f0799ef1
Your Name <you@example.com>
2023-01-17 18:07:13 +0100

    second commit

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,10 @@
 export default function hello(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies
-  });
+  response.status(200).json([
+    {
+      body: request.body,
+      query: request.query,
+      cookies: request.cookies
+    },
+    { name: "test app" }
+  ]);
 }
